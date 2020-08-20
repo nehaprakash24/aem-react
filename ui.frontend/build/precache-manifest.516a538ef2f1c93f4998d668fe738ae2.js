c231700e8114298e54641891a61f6c27afb685fb
Neha Prakash <neha.prakash266@gmail.com>
2020-08-20 23:29:42 +0530

    integrating the spa

---
+++ ui.frontend/build/precache-manifest.516a538ef2f1c93f4998d668fe738ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa7d1d216a070360bed949605c6427f",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "d557d4cb1ecdb0dbbdc7",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/css/main.66ccd4dc.chunk.css"
+  },
+  {
+    "revision": "26e359a559b2b8853cdc",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/2.7d6011ed.chunk.js"
+  },
+  {
+    "revision": "e7d5bbb3f2be49be916aaab2fc075683",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/2.7d6011ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d557d4cb1ecdb0dbbdc7",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/main.66f2de79.chunk.js"
+  },
+  {
+    "revision": "947a28bbb1bf3d46b68d",
+    "url": "/etc.clientlibs/wknd-spa-react/clientlibs/clientlib-react/resources/static/js/runtime-main.b0e1d401.js"
+  }
+]);
